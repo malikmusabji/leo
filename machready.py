e3b4cb6c691a599925e1e78c84867b174ad1b3f2
malikmusabji <117093080+malikmusabji@users.noreply.github.com>
2024-11-24 02:22:15 +0530

    Update machready.py

--- machready.py
+++ machready.py
@@ -15,6 +15,11 @@ genai.configure(api_key=st.secrets["API_KEY"])
 
 def login_page():
     st.subheader("Login")
+    st.markdown("<h1 style='text-align: center; color: #ff5733;'>PedoMUS</h1>", unsafe_allow_html=True)
+    st.markdown("""
+    ### Welcome to PedoMUS
+    **PedoMUS** is a comprehensive educational dashboard that provides an array of tools to support effective learning. The platform allows users to track and manage attendance with ease, interact with uploaded PPTs and PDFs through an intelligent chatbot that can summarize and explain content, and generate multiple-choice questions based on notes to aid in self-assessment. Additionally, PedoMUS offers access to simulation tools that facilitate a deeper understanding of complex concepts. Together, these features create an integrated, user-friendly environment to enrich the educational experience.
+    """)
     user_id = st.text_input("User ID (Numerical Only)")
     password = st.text_input("Password", type="password")
 
