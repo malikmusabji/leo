89e216211e13e023d04fafdaa869a29e6fcd665d
malikmusabji <117093080+malikmusabji@users.noreply.github.com>
2024-11-24 02:15:18 +0530

    ai machready.py

--- machready.py
+++ machready.py
@@ -10,10 +10,8 @@ import bcrypt
 import pytz
 import uuid
 import base64
-from db import init_db
 from datetime import datetime, timedelta
-genai.configure(api_key=st.secrets["API_KEY"])
-init_db()
+genai.configure(api_key=st.secrets["API_KEY"]
 
 def login_page():
     st.subheader("Login")
