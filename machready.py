537528a081eecf53e949a5474d78524e42603665
malikmusabji <117093080+malikmusabji@users.noreply.github.com>
2024-11-24 16:41:36 +0530

    Update machready.py

--- machready.py
+++ machready.py
@@ -407,8 +407,8 @@ def simulation_page():
         """),
         ("Photosynthesis", """
             <iframe src="https://phet.colorado.edu/sims/cheerpj/photoelectric/latest/photoelectric.html?simulation=photoelectric"
-                width="800"
-                height="600"
+                width="1200"
+                height="800"
                 allowfullscreen>
             </iframe>
         """),
